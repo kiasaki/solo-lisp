a17c0a4b41a05c60d1653fa364adfd80c6e0e2f3
Frederic Gingras <frederic@gingras.cc>
2015-03-31 18:28:43 -0400

    Simple REPL (just prints)

---
+++ repl.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+
+int main(int argc, char** argv) {
+  puts("REPL v0.0.1");
+  puts("Press Ctrl+c to exit\n");
+
+  while(1) {
+    char* input = readline("λ: ");
+    add_history(input);
+    printf("okay: %s\n", input);
+    free(input);
+  }
+
+  return 0;
+}
